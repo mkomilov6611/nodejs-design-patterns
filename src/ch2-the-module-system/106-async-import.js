95f3ed22684d7c7c3b7d6e25b46b907ffc49e48e
mkomilov6611 <79241969+mkomilov6611@users.noreply.github.com>
2022-04-26 20:55:53 +0500

    Update 106-async-import.js

--- src/ch2-the-module-system/106-async-import.js
+++ src/ch2-the-module-system/106-async-import.js
@@ -1,1 +1,24 @@
+// strings-uz.js
+export const HELLO = 'Salom'
 
+// strings-en.js
+export const HELLO = 'Hello'
+
+
+// strings-ru.js
+export const HELLO = 'Privet'
+
+// main.js
+const SUPPORTED_LANGUAGES = ['uz', 'en', 'ru']
+const selectedLanguage = process.argv[2]
+
+if(!SUPPORTED_LANGUAGES.includes(selectedLanguage)) {
+  console.error('The specified language is not supported')
+  process.exit(1)
+}
+
+const translationModule = `./strings-${selectedLanguage}.js`;
+
+import(translationModule).then((strings) => {
+  console.log(strings.HELLO);
+});
